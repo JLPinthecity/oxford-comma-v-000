96dc92e76c5c99eb2a0c46a27430165445d2e580
Joann Pan <joannlpan@gmail.com>
2020-05-01 14:56:52 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
-
+def oxford_comma(fruits)
+   if fruits.length == 2
+     return "#{fruits[0]} and #{fruits[1]}"
+   elsif fruits.length > 2
+     fruits.pop
+     
+    
+   else fruits.length == 1
+       return "#{fruits[0]}"
+     
+   end
 end
